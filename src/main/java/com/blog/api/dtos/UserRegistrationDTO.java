9712a3d095caf52da21d4e9355d51434134f289d
Demostenis Villar <ribeiros@outlook.com>
2024-04-28 14:04:17 +0100

    Promote and Revoke admin permission implementation

--- src/main/java/com/blog/api/dtos/UserRegistrationDTO.java
+++ src/main/java/com/blog/api/dtos/UserRegistrationDTO.java
@@ -3,8 +3,9 @@ package com.blog.api.dtos;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
+import java.util.Set;
 import java.util.UUID;
 
 public record UserRegistrationDTO(UUID id, @NotNull String email, @NotNull String password, @NotNull String firstName,
-                                  @NotNull String lastName, @NotNull Date birthDate, String role) {
+                                  @NotNull String lastName, @NotNull Date birthDate, Set<String> roles) {
 }
--- src/main/java/com/blog/api/mappers/UserMapper.java
+++ src/main/java/com/blog/api/mappers/UserMapper.java
@@ -2,10 +2,15 @@ package com.blog.api.mappers;
 
 import com.blog.api.dtos.AuthRequestDTO;
 import com.blog.api.dtos.UserRegistrationDTO;
+import com.blog.data.models.Role;
 import com.blog.data.models.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
@@ -13,6 +18,7 @@ public interface UserMapper {
     @Mapping(target = "person.firstName", source = "firstName")
     @Mapping(target = "person.lastName", source = "lastName")
     @Mapping(target = "person.birthDate", source = "birthDate")
+    @Mapping(target = "roles", ignore = true)
     User toUser(UserRegistrationDTO dto);
 
     @Mapping(target = "id", source = "person.id")
@@ -20,13 +26,20 @@ public interface UserMapper {
     @Mapping(target = "firstName", source = "person.firstName")
     @Mapping(target = "lastName", source = "person.lastName")
     @Mapping(target = "birthDate", source = "person.birthDate")
+    @Mapping(target = "roles", expression = "java(toStringRoles(user.getRoles()))")
     UserRegistrationDTO toUserRegistrationDTO(User user);
 
     User toUser(AuthRequestDTO dto);
+
 
     default UsernamePasswordAuthenticationToken toUsernamePasswordAuthenticationToken(User user) {
         return new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
     }
 
-
+    default Set<String> toStringRoles(Collection<Role> roles) {
+        return roles.stream()
+                .map(Role::getName)
+                .map(String::toUpperCase)
+                .collect(Collectors.toSet());
+    }
 }
